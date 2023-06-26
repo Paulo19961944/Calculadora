df31bb6eec14ed2e8626478523bdd0063aafc43a
Paulo19961944 <106405308+Paulo19961944@users.noreply.github.com>
2023-06-26 13:56:26 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,217 +1,57 @@
-const previousOperationText = document.querySelector("#previous-operations");
-const currentOperationText = document.querySelector("#current-operations");
-const buttons = document.querySelectorAll("#buttons-container button");
-console.log(buttons);
+// Função para adicionar um listener de clique em um elemento
+function addClickListener(element, callback) {
+  element.addEventListener('click', callback);
+}
 
+// Função para atualizar a operação atual na calculadora
+function updateCurrentOperation(value) {
+  const currentOperations = document.getElementById('current-operations');
+  currentOperations.innerText += value;
+}
 
+// Função para limpar a operação atual na calculadora
+function clearCurrentOperation() {
+  const currentOperations = document.getElementById('current-operations');
+  currentOperations.innerText = '';
+}
 
-class Calculator{
-
-    constructor(previousOperationText, currentOperationText){
-        this.previousOperationText = previousOperationText;
-        this.currentOperationText = currentOperationText;
-        this.currentOperation = "";
-    }
-    
-
-    // Adiciona Dígitos a Calculadora
-
-    addDigit(digit){
-
-
-        // Melhora o controle de erros
-
-        if (digit === "." && this.currentOperation === "") {
-            this.currentOperation = "0";
-        } else if (digit === "." && this.currentOperation.includes(".")) {
-            return;
-        }
-        
-
-        // Checar se tem o Current Operation tem um ponto
-        if(digit === "." && this.currentOperationText.innerText.includes(".")){
-            return;
-        }
-
-        this.currentOperation = digit;
-        this.updateScreen();
-    }
-
-    // Processar todos os Dados da Calculadora
-
-    processOperation(operation){
-
-
-        // Resolve conflitos entre operações
-
-        if (
-            ["+", "-", "*", "/"].includes(operation) &&
-            ["+", "-", "*", "/"].includes(this.currentOperationText.innerText.slice(-1))
-        ) {
-            this.currentOperationText.innerText = this.currentOperationText.innerText.slice(
-                0,
-                -1
-            );
-        }
-        
-
-        // Checar se current está vazio
-        if(currentOperationText.innerText === "" && operation !== "C"){
-            if(this.previousOperationText.innerText !== ""){
-                // Mudança de Operação
-                this.changeOperation(operation);
-
-            }
-            
-            return;
-
-        } 
-    
-        
-        // Get current and previous value
-        let operationValue
-        const previous = +this.previousOperationText.innerText.split(" ")[0];
-        const current = +this.currentOperationText.innerText;
-
-        switch(operation){
-           
-            case "+":
-            operationValue = previous + current;
-                this.currentOperation = "";
-                this.updateScreen(operationValue, operation, current, previous);
-                break;
-            case "-":
-                operationValue = previous - current;
-                this.currentOperation = "";
-                this.updateScreen(operationValue, operation, current, previous);
-                break;
-            case "/":
-                operationValue = previous / current;
-                this.currentOperation = "";
-                this.updateScreen(operationValue, operation, current, previous);
-             break;
-            case "*":
-                operationValue = previous * current;
-                this.currentOperation = "";
-                this.updateScreen(operationValue, operation, current, previous);
-                break;
-            case "=":
-                this.processEqualOperator();
-                break;
-            case "DEL":
-                this.processDelOperator();
-                break;
-            case "CE":
-                this.processClearCurrentOperation();
-                break;
-            case "C":
-                this.processClearOperation();
-                break;
-            
-        }
-
-
-    }
-    // Mudando valores da tela da Calculadora
-    updateScreen(
-        operationValue = null,
-        operation = null,
-        current = null,
-        previous = null
-        ) {
-
-        console.log(operationValue, operation, current, previous)
-        
-        if(operationValue === null){
-            this.currentOperationText.innerText += this.currentOperation;
-        }
-
-        else{
-            
-            // Checar se o valor é zero, se for, apenas adicione currentValue
-
-            if(previous === 0){
-                operationValue = current
-            }
-
-            // Adicione currentValue para previous
-            this.previousOperationText.innerText = `${operationValue} ${operation}`;
-            this.currentOperationText.innerText = "";
-            
-
-
-        }
-    }
-    
-  // Mudando operaçoes matematicas
+// Função para calcular o resultado da operação
+function calculate() {
+  const currentOperations = document.getElementById('current-operations');
+  const previousOperations = document.getElementById('previous-operations');
   
-    changeOperation(operation){
-    const mathOperations = ["*", "/", "+", "-"]
-
-    if(!mathOperations.includes(operation)){
-        return
-    }
-
-    this.previousOperationText.innerText = this.previousOperationText.innerText.slice(0, -1) + operation;
-
-    
-  }  
-
-  // Apagar o ultimo digito
-
-  processDelOperator(){
-    this.currentOperationText.innerText = this.currentOperationText.innerText.slice(0, -1);
-  }
-
-  processClearCurrentOperation(){
-    this.currentOperationText.innerText = "";
-  }
-
-  processClearOperation(){
-    this.currentOperationText.innerText = "";
-    this.previousOperationText.innerText = "";
-  }
-
-  // Resultado
-
-  processEqualOperator(){
-    const operation = previousOperationText.innerText.split(" ")[1]
-    this.processOperation(operation)
-
+  try {
+    const result = eval(currentOperations.innerText.replace(',', '.'));
+    previousOperations.innerText = currentOperations.innerText + ' =';
+    currentOperations.innerText = result.toString().replace('.', ',');
+  } catch (error) {
+    currentOperations.innerText = 'Erro';
+    console.error(error);
   }
 }
 
-const calc = new Calculator(previousOperationText, currentOperationText);
+// Função para inicializar a calculadora
+function initializeCalculator() {
+  const numberButtons = document.getElementsByClassName('number');
+  const equalButton = document.getElementById('equal-btn');
+  const clearButton = document.getElementById('C');
+  const deleteButton = document.getElementById('DEL');
 
-buttons.forEach((btn)=> {
-    btn.addEventListener("click", (e) => {
-        const value = e.target.innerText;
+  for (let i = 0; i < numberButtons.length; i++) {
+    addClickListener(numberButtons[i], function () {
+      updateCurrentOperation(numberButtons[i].innerText);
+    });
+  }
 
-        if (+value >=0 || value === "."){
-            calc.addDigit(value)
-        }
+  addClickListener(equalButton, calculate);
+  addClickListener(clearButton, clearCurrentOperation);
+  addClickListener(deleteButton, function () {
+    const currentOperations = document.getElementById('current-operations');
+    currentOperations.innerText = currentOperations.innerText.slice(0, -1);
+  });
+}
 
-        else{
-            calc.processOperation(value)
-        }
-
-    } )
-})
-
-
-// Adiciona teclas de atalhos ao teclado
-
-document.addEventListener("keydown", (event) => {
-    const key = event.key;
-    if (+key >= 0 || key === ".") {
-        calc.addDigit(key);
-    } else if (key === "+" || key === "-" || key === "*" || key === "/") {
-        calc.processOperation(key);
-    } else if (key === "=") {
-        calc.processEqualOperator();
-    } else if (key === "Backspace") {
-        calc.processDelOperator();
-    } else if (key === "Escape") {
-        calc.processClearOperation();
-    }
+// Inicializar a calculadora quando a página estiver carregada
+document.addEventListener('DOMContentLoaded', function () {
+  initializeCalculator();
 });
