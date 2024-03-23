428837ab7c88b35e3608bb8179a631b56a579b69
Paulo19961944 <paulo24041996@gmail.com>
2024-03-23 15:17:33 -0300

    Calculadora V1.0.6

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,88 +1,46 @@
-// Captura as Entradas HTML
-const display = document.querySelector(".display");
-const keys = document.querySelector(".keys");
-const numbers = document.querySelectorAll(".numbers");
-const operators = document.querySelectorAll(".operators")
-const equalBtn = document.getElementById("equal");
-const cleanAll = document.getElementById("clean-all");
-const cleanNumber = document.getElementById("clean-number");
+// Captura os elementos do HTML
+let display = document.querySelector('.display')
+const keys = document.querySelectorAll('.keys')
+const numbers = document.querySelectorAll('.numbers')
+const operators = document.querySelectorAll('.operators')
+const equalBtn = document.querySelector('.equal')
+const clearAll = document.querySelector('.clear-all')
+const clearNumber = document.querySelector('.clear-number')
+let result = null; // Declara o Resultado
 
 // Variáveis
-let firstNumber = null;
-let secondNumber = null;
-let operatorCurrent = null;
-let result = 0;
+let operatorValue = null;
 
-// Aparece o numero do Display na Tela
-numbers.forEach(numberValue => {
-    numberValue.addEventListener("click", () => {
-        display.textContent += numberValue.textContent;
+// Adiciona ao display o que foi digitado
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        display.textContent += number.textContent
     });
 });
 
-// Lógica dos Operadores
-operators.forEach(numberValue => {
-    numberValue.addEventListener("click", () => {
-        operatorCurrent = numberValue.textContent;
-        if (firstNumber === null) {
-            firstNumber = parseFloat(display.textContent);
-        } else if (secondNumber === null) {
-            secondNumber = parseFloat(display.textContent);
-            Calcular();
-            console.log(result)
-        }
-        display.textContent = "";
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        display.textContent +=  operator.textContent
+        operatorValue = operator.textContent;
     });
 });
 
-// Limpa toda a Tela
-cleanAll.addEventListener("click", () => {
-    display.textContent = "";
-    firstNumber = null;
-    secondNumber = null;
-    operatorCurrent = null;
-    result = 0;
+clearAll.addEventListener('click', () => {
+    display.textContent = ''
+})
+
+clearNumber.addEventListener('click', () => {
+    let displayValue = display.textContent;
+    let lastIndexOperator = displayValue.lastIndexOf(operatorValue);
+    display.textContent = displayValue.slice(0, lastIndexOperator);
 });
 
-// Limpa os Numeros
-cleanNumber.addEventListener("click", () => {
-    if(operatorCurrent !== null){
-        let displayValue = display.textContent;
-        let lastIndexOperator = displayValue.lastIndexOf(operatorCurrent);
-        display.textContent = displayValue.substring(0, lastIndexOperator + 1);
-        secondNumber = null;
-    } else{
-        cleanAll();
-    }
-});
-
-// Botão de Igual
 equalBtn.addEventListener("click", () => {
-    secondNumber = parseFloat(display.textContent);
-    Calcular();
-    display.textContent = "";
-    display.textContent += result.toFixed(2);
-});
-
-// Função Calcular
-function Calcular() {
-    switch (operatorCurrent) {
-        case "+":
-            result = firstNumber + secondNumber;
-            break;
-        case "-":
-            result = firstNumber - secondNumber;
-            break;
-        case "*":
-            result = firstNumber * secondNumber;
-            break;
-        case "/":
-            if (secondNumber === 0) {
-                alert("Impossivel dividir por Zero!")
-            } else {
-                result = firstNumber / secondNumber;
-            } break;
+    try {
+        result = eval(display.textContent);
+        display.textContent = result;
+    } catch (error) {
+        // Se ocorrer um erro ao avaliar a expressão, exiba uma mensagem de erro
+        alert('Erro na expressão!');
     }
-    firstNumber = result;
-    secondNumber = null;
-}
+});
