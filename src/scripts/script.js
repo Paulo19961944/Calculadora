5de2bd778930a672b92a153e503bd9e84e6ad975
Paulo19961944 <paulo24041996@gmail.com>
2024-03-23 18:26:13 -0300

    Calculadora V1.1.0

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,12 +1,11 @@
 // Captura os elementos do HTML
-let display = document.querySelector('.display') // Captura o Display do HTML
-const keys = document.querySelectorAll('.keys')  // Captura as Teclas do HTML 
-const numbers = document.querySelectorAll('.numbers') // Captura todos os números do HTML
-const operators = document.querySelectorAll('.operators') // Captura todos os operadores do HTML
-const equalBtn = document.querySelector('.equal') // Captura o botão de igual
-const clearAll = document.querySelector('.clear-all') // Captura o C do HTML
-const clearNumber = document.querySelector('.clear-number') // Captura o CE do HTML
-let result = null; // Declara o Resultado
+const display = document.querySelector('.display'); // Captura o Display do HTML
+const keys = document.querySelectorAll('.keys'); // Captura as Teclas do HTML 
+const numbers = document.querySelectorAll('.numbers'); // Captura todos os números do HTML
+const operators = document.querySelectorAll('.operators'); // Captura todos os operadores do HTML
+const equalBtn = document.querySelector('.equal'); // Captura o botão de igual
+const clearAll = document.querySelector('.clear-all'); // Captura o C do HTML
+const clearNumber = document.querySelector('.clear-number'); // Captura o CE do HTML
 let operatorValue = null; // Variável para armazenar o último operador
 
 // Função para adicionar valor ao display
@@ -14,22 +13,22 @@ function addToDisplay(value) {
     display.textContent += value; // Adiciona valor ao display
 }
 
-function clearDisplay(){
-    display.textContent = ''
+function clearDisplay() {
+    display.textContent = '';
 }
 
 // Função para avaliar a expressão e exibir o resultado
 function calculate() {
     try {
         const expression = display.textContent;
-        const evaluate = new Function('return ' + expression); // Cria uma função a partir da expressão
-        result = evaluate(); // Avalia a expressão
+        const sanitizedExpression = expression.replace(/[^-()\d/*+.]/g, ''); // Sanitiza a expressão permitindo apenas caracteres válidos
+        const evaluate = new Function('return ' + sanitizedExpression); // Cria uma função a partir da expressão sanitizada
+        const result = evaluate(); // Avalia a expressão
         display.textContent = result; // Exibe o resultado na tela
     } catch (error) {
-        alert('Erro na expressão!');
+        display.textContent = 'Erro'; // Exibe uma mensagem de erro genérica
     }
 }
-
 
 // Adiciona ao display o que foi digitado
 numbers.forEach(number => {
@@ -51,11 +50,14 @@ clearAll.addEventListener('click', () => {
     clearDisplay(); // Chama a função para limpar a tela
 });
 
-// Limpa apenas o número
+// Limpa apenas o último número ou operador
 clearNumber.addEventListener('click', () => {
-    let displayValue = display.textContent; // Cria variável pra pegar o valor do display
-    let lastIndexOperator = displayValue.lastIndexOf(operatorValue); // Retorna o ultimo indice do operador
-    display.textContent = displayValue.slice(0, lastIndexOperator); // Apaga tudo que estiver até o ultimo indice do operador
+    let displayValue = display.textContent.trim(); // Obtém o valor do display sem espaços em branco no início e no fim
+    const lastChar = displayValue.slice(-1); // Obtém o último caractere da expressão
+    if (operators.includes(lastChar)) {
+        operatorValue = null; // Limpa o valor do último operador
+    }
+    display.textContent = displayValue.slice(0, -1); // Remove o último caractere da expressão
 });
 
 // Evento ao botão de igual
@@ -66,12 +68,12 @@ equalBtn.addEventListener("click", () => {
 // Evento keydown para capturar as teclas pressionadas
 document.addEventListener('keydown', event => {
     const { key } = event;
-    // Verifica se a tecla pressionada é um número, operador ou Enter (para calcular)
-    if (!isNaN(parseInt(key)) || key === '+' || key === '-' || key === '*' || key === '/' || key === '.' || key === '(' || key === ')') {
-        addToDisplay(key); // Adiciona ao display
+    const validKeys = /^[0-9+\-*/().]$/; // Expressão regular para validar as teclas permitidas
+    if (validKeys.test(key)) {
+        addToDisplay(key); // Adiciona ao display se a tecla pressionada for válida
     } else if (key === 'Enter') {
-        calculate(); // Calcula
+        calculate(); // Calcula se a tecla pressionada for Enter
     } else if (key === 'Backspace') {
-        display.textContent = display.textContent.slice(0, -1) // Apaga o numero anterior
+        clearNumber.click(); // Aciona o evento de limpar apenas o último número ou operador se a tecla pressionada for Backspace
     }
 });
